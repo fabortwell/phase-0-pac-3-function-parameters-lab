34b61e0d980de5bb7fa600151ec73fa5a00e6e88
Brian Sitati <briansit93@gmail.com>
2022-04-01 21:04:27 +0300

    edited a function parameter

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function introduction(name) {
+    return ('Hi, my name is Aki.');
+  }
+
+  
+
+  function introductionWithLanguage(name, langauge) {
+    return ('Hi, my name is Aki.' + 'I am learning to
+    program in Javascript');
+  }
